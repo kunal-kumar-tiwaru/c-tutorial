8222338fcc17ec1d090a1c880410d75c622c491c
Kunal Kumar Tiwari <kunalkumartiwari2468@gmail.com>
2026-08-11 17:01:56 +0530

    area of square ok

---
+++ area of square.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int side;
+    printf("enter side of square: ");
+    scanf("%d", &side);
+
+    int area = side *side;
+
+    printf("area of square is %d",area);
+    return 0;
+
+
+}
