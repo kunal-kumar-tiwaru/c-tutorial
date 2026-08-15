642900ad77d04741c85bef5ec3e2e66a3290dee5
Kunal Kumar Tiwari <kunalkumartiwari2468@gmail.com>
2026-08-15 14:32:49 +0530

    simple calculator

---
+++ simple calcularer.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+
+    float num1;
+    scanf("%f", &num1);
+     
+    char sym;
+    scanf(" %c", &sym);
+
+    float num2;
+    scanf("%f", &num2);
+
+    switch (sym) {
+        case '+':printf("%f", num1 + num2);
+            break;
+        case '-':printf("%f", num1 - num2);
+            break;
+        case '*':printf("%f", num1 * num2);
+            break;
+        case '/':printf("%f", num1 / num2);
+            break;
+        case '^':printf("%f", pow(num1, num2));
+            break;
+        default:printf("some error found");
+    }
+
+    return 0;
+
+}
+
