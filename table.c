41ce9a5034bdcf3071b537de94a2e78b5ea390b4
Kunal Kumar Tiwari <kunalkumartiwari2468@gmail.com>
2026-08-25 16:52:19 +0530

    Add multiplication table generator in table.c
    This commit introduces a new file, table.c, which contains a simple program that prompts the user for a number and prints its multiplication table from 1 to 10. The program utilizes a for loop to iterate through the multipliers and displays the results in a formatted manner.

---
+++ table.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int num;
+    printf("enter the number");
+    scanf("%d", &num);
+
+    for (int i = 1; i <= 10; i += 1) {
+        printf("%d * %d = %d\n", num, i, num * i);
+    }
+    
+    return 0;
+
+
+
+
+}
