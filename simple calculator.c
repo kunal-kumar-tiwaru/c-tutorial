82f994aea287ad2f7940280272377386041fd173
Kunal Kumar Tiwari <kunalkumartiwari2468@gmail.com>
2026-09-04 16:15:54 +0530

    Refactor code structure for improved readability and maintainability

--- simple calculator.c
+++ simple calculator.c
@@ -2,37 +2,9 @@
 #include<math.h>
 
 int main(){
-
-   float num1;
-    scanf("%f", &num1);
-     
-    char sym;
-    scanf(" %c", &sym);
-
-    float num2;
-    scanf("%f", &num2);
-
-    switch (sym) {
-        case '+':printf("%f", num1 + num2);
-            break;
-        case '-':printf("%f", num1 - num2);
-            break;
-        case '*':printf("%f", num1 * num2);
-            break;
-        case '/':
-            if(num2 == 0){
-                printf("any number is not devided by 0");
-            } 
-            else {
-                printf("%f", num1 / num2);
-            }
-            break;
-        case '^':printf("%f", pow(num1, num2));
-            break;
-        default:printf("some error found");
-    }
-
+    float a,b,c,d,e,f,g;
+    printf("Enter marks of subjects in order of maths,physics,english,physical education,chemistry:")
+    scanf("%f","%f","%f","%f","%f", &a ,&b ,%c , %d, %e);
     return 0;
-
 }
 
